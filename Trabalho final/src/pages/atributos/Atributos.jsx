934e5b35ce867683809211fecf0f5138ada5faa0
BRENDOOM999 <66705424+BRENDOOM999@users.noreply.github.com>
2022-06-06 21:00:45 -0300

    Update Atributos.jsx

--- Trabalho final/src/pages/atributos/Atributos.jsx
+++ Trabalho final/src/pages/atributos/Atributos.jsx
@@ -6,6 +6,7 @@ import { BsArrowLeft } from 'react-icons/bs'
 import AtributosService from '../../services/rpg/AtributosService';
 import atributosValidator from '../../validators/atributosValidator';
 import { Link, useNavigate, useParams } from 'react-router-dom';
+import { mask } from 'remask'
 
 const Atributos = () => {
 
@@ -23,15 +24,20 @@ const Atributos = () => {
     }
   }, [])
 
+  function handleChange(event) {
+    const mascara = event.target.getAttribute('mask')
+    setValue(event.target.name, mask(event.target.value, mascara))
+  }
+
   function salvar(dados) {
 
     if (params.id) {
         AtributosService.update(params.id, dados)
+        navigate('/fichasfinais')
     } else {
         AtributosService.create(dados)
+        navigate('/proficienciaseidiomas/create')
     }
-
-    navigate('/proficienciaseidiomas/:id')
   }
 
   return (
@@ -41,31 +47,36 @@ const Atributos = () => {
       <Form >
         <Form.Group className="mb-3" controlId="forca">
           <Form.Label>Força: </Form.Label>
-          <Form.Control isInvalid={errors.forca} type="text" {...register("forca", atributosValidator.forca)} />
+          <Form.Control isInvalid={errors.forca} type="text" {...register("forca", atributosValidator.forca)} 
+           mask="99" onChange={handleChange}/>
           {errors.forca && <span>{errors.forca.message}</span>}
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="destreza">
           <Form.Label>Destreza: </Form.Label>
-          <Form.Control isInvalid={errors.destreza} type="text" {...register("destreza", atributosValidator.destreza)} />
+          <Form.Control isInvalid={errors.destreza} type="text" {...register("destreza", atributosValidator.destreza)} 
+           mask="99" onChange={handleChange}/>
           {errors.destreza && <span>Campo Obrigatório</span>}
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="constituicaoa">
           <Form.Label>Costituição: </Form.Label>
-          <Form.Control isInvalid={errors.constituicaoa} type="text" {...register("constituicaoa", atributosValidator.constituicaoa)} />
+          <Form.Control isInvalid={errors.constituicaoa} type="text" {...register("constituicaoa", atributosValidator.constituicaoa)} 
+           mask="99" onChange={handleChange}/>
           {errors.constituicaoa && <span>Campo Obrigatório</span>}
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="inteligencia">
           <Form.Label>Inteligência: </Form.Label>
-          <Form.Control isInvalid={errors.inteligencia} type="text" {...register("inteligencia", atributosValidator.inteligencia)} />
+          <Form.Control isInvalid={errors.inteligencia} type="text" {...register("inteligencia", atributosValidator.inteligencia)} 
+           mask="99" onChange={handleChange}/>
           {errors.inteligencia && <span>Campo Obrigatório</span>}
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="sabedoria">
           <Form.Label>Sabedoria: </Form.Label>
-          <Form.Control isInvalid={errors.sabedoria} type="text" {...register("sabedoria", atributosValidator.sabedoria)} />
+          <Form.Control isInvalid={errors.sabedoria} type="text" {...register("sabedoria", atributosValidator.sabedoria)} 
+           mask="99" onChange={handleChange}/>
           {errors.sabedoria && <span>Campo Obrigatório</span>}
         </Form.Group>
 
@@ -73,7 +84,6 @@ const Atributos = () => {
         
         <div className="text-center">
           <Button onClick={handleSubmit(salvar)} className='btn btn-success'><FaCheck /> Salvar</Button>{' '}
-          <Link className='btn btn-danger' to={-1}><BsArrowLeft /> Voltar</Link>
         </div>
       </Form>
 
